ff015b56d1e334220dc8b71b855af0a500b899e4
Harmandeep Singh <harmandeepsingh02764@gmail.com>
2025-05-01 15:49:30 +0530

    second commit

--- main.js
+++ main.js
@@ -16,30 +16,18 @@ navBar.forEach(function(a){
         navCollapse.classList.remove("show");
     })
 })
-const tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="tooltip"]'));
-tooltipTriggerList.map(function (tooltipTriggerEl) {
-  return new bootstrap.Tooltip(tooltipTriggerEl);
-});
 
-const popoverTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="popover"]'));
-popoverTriggerList.map(function (popoverTriggerEl) {
-  return new bootstrap.Popover(popoverTriggerEl);
-});
-
-document.addEventListener("DOMContentLoaded", function () {
-  const appointmentForm = document.getElementById("appointmentForm");
-  const appointmentAlert = document.getElementById("appointmentAlert");
-
-  appointmentForm.addEventListener("submit", function (e) {
-    e.preventDefault(); // Prevent default form behavior
-
-    // Show the alert properly
-    appointmentAlert.classList.remove("d-none");
-    appointmentAlert.classList.add("show");
-
-    // Reset the form
-    appointmentForm.reset();
+  $(document).ready(function() {
+    var departments = ["Cardiology", "Neurology", "Orthopedics", "Pediatrics", "Dermatology"];
+    
+    $('#department').typeahead({
+      source: departments,
+      autoSelect: true
+    });
   });
-});
+
+
+
+
 
 
